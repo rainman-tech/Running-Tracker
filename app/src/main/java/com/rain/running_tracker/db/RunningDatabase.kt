6908009e056fc88e9b24a4f8c57f02ce358722f8
rainman-tech <reynflores890@gmail.com>
2021-06-28 20:20:42 +0800

    Room database setup. -Entities -Data Access Object -Database class -Type Converters

---
+++ app/src/main/java/com/rain/running_tracker/db/RunningDatabase.kt
@@ -0,0 +1,17 @@
+package com.rain.running_tracker.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [Run::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
+abstract class RunningDatabase : RoomDatabase() {
+
+    abstract fun getRunDao(): RunDao
+
+}
